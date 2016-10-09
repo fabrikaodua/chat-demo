a35bf70709155c4d568e32c239baffcc61997585
constgen <const.gen@gmail.com>
2016-10-09 16:10:53 +0300

    add support of sent time

--- src/modules/message.controller.js
+++ src/modules/message.controller.js
@@ -8,8 +8,12 @@ var chatElem = $('.chat')
 function addMesasge(message) {
 	var content = message.content
 	var userName = message.user.name
+	var time = new Date(message.timeStamp)
 	var html = '<div class="message">'
-		+ '<h5 class="user-name">' + userName + '</h5>'
+		+ '<h5 class="user-name">'
+			+ userName
+			+ '<span class="time">' + time + '</span>'
+		+ '</h5>'
 		+ '<p class="content">' + content + '</p>'
 	+ '</div>'
 	var messageElem = $(html)

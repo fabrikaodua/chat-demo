2811617dc933c3b8808ec923779ec3cdb0a04ba5
constgen <const.gen@gmail.com>
2016-10-09 13:46:19 +0300

    chat via console

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
 var $ = require('jquery'),
-	lodash = require('lodash'),
-	pubnub = require('pubnub');
+	lodash = require('lodash');
+
+var pubnub = require('./pubnub.js')
+
+window.send = pubnub.sendMessage
+
+pubnub.onMessageRecieved(function(message){
+	console.log(message)
+})
